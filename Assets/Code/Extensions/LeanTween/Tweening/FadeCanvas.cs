3488007c04e647012d47ae3806ba8dafa6e3f20b
pablo mansilla martinelli <pablo.0698@hotmail.com>
2024-10-17 19:49:13 -0500

    created project with basic configuration

---
+++ Assets/Code/Extensions/LeanTween/Tweening/FadeCanvas.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace UI.Effects
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class FadeCanvas : Fade
+    {
+        [Header("Canvas Group")]
+        [SerializeField] private bool _affectRaycast;
+        [SerializeField] private bool _affectInteraction;
+
+        [HideInInspector, SerializeField] private CanvasGroup _group;
+        protected override float _alpha { get => _group.alpha; set => _group.alpha = value; }
+
+        private void Awake() => _group = GetComponent<CanvasGroup>();
+        private void OnEnable() => _onFade += OnFadeComplete;
+        private void OnDisable() => _onFade -= OnFadeComplete;
+
+        public void Default()
+        {
+            _alpha = 1;
+            OnFadeComplete(true);
+        }
+        private void OnFadeComplete(bool value)
+        {
+            if (_affectInteraction) _group.interactable = value;
+            if (_affectRaycast) _group.blocksRaycasts = value;
+        }
+    }
+}
---
+++ Assets/Code/System/Scenes/SceneController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UI.Effects;
+
+namespace UnityEngine.SceneManagement
+{
+    [AddComponentMenu("System/SceneManagement/SceneController")]
+    public class SceneController : SingletonBasic<SceneController>
+    {
+        [SerializeField] private AudioSource _source;
+        [SerializeField] private FadeScene _fade;
+
+        public List<string> scenes { get; protected set; }
+        public Action onSwitchScene;
+        private bool _lock;
+
+        protected override void Awake() { base.Awake(); scenes = new(); }
+        public void CutScene(string value) => SceneManager.LoadScene(value);
+        public void SwipeScene(string value) => OnFading(value);
+        public void Quit() => OnFading(string.Empty);
+
+        public IEnumerator AddScene(string value)
+        {
+            if (scenes.Contains(value)) yield break;
+            yield return SceneManager.LoadSceneAsync(value, LoadSceneMode.Additive);
+            scenes.Add(value);
+        }
+        public void RemoveScene(string value)
+        {
+            if (!scenes.Contains(value)) return;
+            SceneManager.UnloadSceneAsync(value, UnloadSceneOptions.None);
+            scenes.Remove(value);
+        }
+        private void OnFading(string value)
+        {
+            if (_lock) return; else _lock = true; onSwitchScene?.Invoke();
+            FadeScene fade = Instantiate(_fade, transform);
+            fade.onComplete += onComplete;
+            fade.onUpdate += onUpdate;
+
+            void onUpdate(float value) { if (_source) _source.volume = 1 - value; }
+            void onComplete()
+            {
+                if (string.IsNullOrEmpty(value)) Application.Quit();
+                else SceneManager.LoadSceneAsync(value, LoadSceneMode.Single);
+            }
+        }
+    }
+}
