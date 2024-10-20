59daa997e93a7d05ef2625d11da109362efdd3d8
pablo mansilla martinelli <pablo.0698@hotmail.com>
2024-10-20 11:05:20 -0500

    change spawn logic with instance polling on football minigame

--- Assets/Code/Scripts/Events/Interactions/Spawn.cs
+++ Assets/Code/Scripts/Events/Interactions/Spawn.cs
@@ -5,7 +5,29 @@ namespace Events.Interact
     public class Spawn : MonoBehaviour
     {
         [SerializeField] private GameObject _item;
+        [SerializeField] private int _length;
 
-        public void SpawnObject() => Instantiate(_item, transform.position, transform.rotation);
+        private Transform _transform;
+
+        private void Awake() => _transform = transform;
+
+        public void SpawnObject()
+        {
+            GameObject obj = null;
+            int count = _transform.childCount;
+
+            for (int i = 0; i < count; i++) {
+                Transform child = _transform.GetChild(i);
+                if (!child.gameObject.activeInHierarchy) { obj = child.gameObject; break; }
+            }
+
+            if (obj == null) {
+                if (count >= _length) return;
+                obj = Instantiate(_item, _transform);
+            }
+
+            obj.SetActive(true);
+            obj.transform.SetPositionAndRotation(_transform.position, _transform.rotation);
+        }
     }
 }
