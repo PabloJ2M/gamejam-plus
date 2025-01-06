8d72c472db625872b34917cad71964a3fc3fe115
unknown <TIP@CHCOUNTERCPU01.campustls.local>
2025-01-06 11:00:55 -0500

    fixed screen movement

--- Assets/Code/System/Cinemachine/CameraConfiner.cs
+++ Assets/Code/System/Cinemachine/CameraConfiner.cs
@@ -1,21 +1,28 @@
 using UnityEngine;
+using Unity.Mathematics;
 
 namespace Unity.Cinemachine
 {
     [RequireComponent(typeof(CinemachineConfiner2D))]
     public class CameraConfiner : MonoBehaviour
     {
+        [SerializeField] private Camera _camera;
         private CinemachineConfiner2D _confiner2D;
+        private Vector2 _size;
 
         private void Awake() => _confiner2D = GetComponent<CinemachineConfiner2D>();
+        private void Start() => _size = _camera.ViewportToWorldPoint(Vector2.one);
         private void LateUpdate()
         {
             if (_confiner2D.BoundingShape2D == null) return;
 
             Vector3 position = transform.position;
-            Vector2 confinedPosition = _confiner2D.BoundingShape2D.ClosestPoint(position);
+            Bounds area = _confiner2D.BoundingShape2D.bounds;
 
-            transform.position = new(confinedPosition.x, confinedPosition.y, position.z);
+            position.x = math.clamp(position.x, area.min.x + _size.x, area.max.x - _size.x);
+            position.y = math.clamp(position.y, area.min.y + _size.y, area.max.y - _size.y);
+            
+            transform.position = position;
         }
     }
 }
