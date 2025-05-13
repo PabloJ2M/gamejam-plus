6adb0c9a452771a634dd07683d59db8cf47aa551
pablo mansilla martinelli <pablo.0698@hotmail.com>
2025-05-12 19:37:41 -0500

    fixed double render in tutorial & chat ia

--- Assets/Code/System/IA/AIBehaviour.cs
+++ Assets/Code/System/IA/AIBehaviour.cs
@@ -7,7 +7,7 @@ public abstract class AIBehaviour : MonoBehaviour
     protected OpenAIApi OpenAIApi = new();
     protected List<ChatMessage> messages = new();
 
-    private const string _key = "";
+    private const string _key = "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";
 
     protected virtual void Start() => OpenAIApi = new OpenAIApi(_key);
 }
